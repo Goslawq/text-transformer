7758198127d0ef99b212f88c6c32a08e1404482c
Eliza Czaplewska <czaplewska.eliza@gmail.com>
2020-12-09 20:52:00 +0100

    Added LowerUpperDecorator
    Class has 3 modes.
    Lowering, uppering and capitalizing.

---
+++ src/main/java/pl/put/poznan/transformer/logic/LowerUpperDecorator.java
@@ -0,0 +1,43 @@
+package pl.put.poznan.transformer.logic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class has 3 modes. Depending on input int value it will upper, lower or capitalize the input.
+ * This class extends TextInterfaceDecorator
+ */
+
+public class LowerUpperDecorator extends TextInterfaceDecorator {
+    private final int mode;
+    public LowerUpperDecorator(TextInterface text_input_instance, int mode) {
+        super(text_input_instance);
+        this.mode = mode;
+    }
+
+    /**
+     * Method to get text after the desired transformation
+     * @return String
+     */
+
+    @Override
+    public String getTransformedText() {
+        Logger logger = LoggerFactory.getLogger("UpperLowerCapitalize");
+        logger.debug("Computing deeper transformations");
+        String input = super.getTransformedText();
+        String output = null;
+        logger.debug("Got input: " + input);
+        if (this.mode == 0) {
+            logger.debug("Upper all characters");
+            output = input.toUpperCase();
+        } else if (this.mode == 1) {
+            logger.debug("Lower all characters");
+            output = input.toLowerCase();
+        } else if (this.mode ==2) {
+            logger.debug("Capitalize input");
+            output = input.substring(0, 1).toUpperCase() + input.substring(1);
+        }
+        logger.debug("Returning output: " + output);
+        return output;
+    };
+}
