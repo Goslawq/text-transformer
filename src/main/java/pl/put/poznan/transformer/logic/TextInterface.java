745874e064a677bb487abf6ad2ae92d6a4ae7d53
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-11 16:31:29 +0000

    Deploy Goslawq/text-transformer to github.com/Goslawq/text-transformer.git:gh-pages

--- src/main/java/pl/put/poznan/transformer/logic/TextInterface.java
+++
@@ -1,9 +0,0 @@
-package pl.put.poznan.transformer.logic;
-
-/**
- * text interface required for TextInterfaceDecorator and ConcreteText
- */
-public interface TextInterface {
-    String getTransformedText();
-}
-
