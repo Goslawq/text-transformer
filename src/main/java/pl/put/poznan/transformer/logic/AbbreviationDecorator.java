d038480f5e488cf5d3485178b533e37f1cf96ec5
szymon8576 <50836217+szymon8576@users.noreply.github.com>
2020-12-20 00:37:29 +0100

    Update AbbreviationDecorator.java
    updated JavaDoc comments and handling word boundaries while replacing them

--- src/main/java/pl/put/poznan/transformer/logic/AbbreviationDecorator.java
+++ src/main/java/pl/put/poznan/transformer/logic/AbbreviationDecorator.java
@@ -11,11 +11,11 @@ import java.util.Scanner;
 
 /***
  * Class that shortens words to their abbreviations and extends them to full form.
- * Takes boolean 'mode' attribute which specify operation that should be performed:
- * mode = true - extend abbreviations
- * mode = false - shorten words to abbreviations
+ * Takes boolean 'mode' attribute specifying operation that should be performed:
+ *    mode = true - extend abbreviations (for example "Mgr, dr, prof. to przykładowe stopnie naukowe" should be transformed to "Magister, doktor, profesor to przykładowe stopnie naukowe")
+ *    mode = false - shorten words to abbreviations (for example "Między innymi doktor magister Nowak jest tu dziś nami" should be transformed to "M. in. dr mgr. Nowak jest tu dziś z nami")
  * The dictionary file (user_dictionary.txt), containing words to shorten/extend
- * is placed in the main directory of this program - feel free to edit it so that the program lives up to your expectations.
+ * is placed in the main directory of this project - feel free to edit it so that the application lives up to your expectations.
  * All words in user_dictionary should be in lowercase - replacement will be performed automatically also with their first-capital-letter form.
  * It extends TextInterfaceDecorator.
  * @author Szymon
@@ -26,6 +26,12 @@ public class AbbreviationDecorator extends TextInterfaceDecorator{
     private static final Logger logger = LoggerFactory.getLogger(AbbreviationDecorator.class);
     private final boolean mode;
 
+    /***
+     *
+     * @param text_input Text to be transformed
+     * @param mode Boolean value specifying working mode (true - extend abbreviations, false - shorten words to their abbreviations)
+     *
+     */
 
     public AbbreviationDecorator (TextInterface text_input,boolean mode)
     {
@@ -36,7 +42,6 @@ public class AbbreviationDecorator extends TextInterfaceDecorator{
 
     /***
      * The method that performs desired transformation.
-     * There are two modes avaliable.
      * @return Transformed String
      */
 
@@ -60,7 +65,8 @@ public class AbbreviationDecorator extends TextInterfaceDecorator{
             }
             myReader.close();
         } catch (FileNotFoundException e) {
-            logger.debug("An error occured while opening file.");
+            logger.debug("An error occurred while opening file.");
+            return text;
         }
 
         for (Map.Entry<String, String> entry : words_map.entrySet()) {
@@ -70,19 +76,20 @@ public class AbbreviationDecorator extends TextInterfaceDecorator{
 
             //if mode==True (extend)
             if(mode){
-                //escape dot character and add boundaries, \W in regex means [^a-zA-Z0-9_] (to prevent replacements such as 'drużyna'->'doktorużyna')
-                String from_a1="[\\W]"+from.replace(".","\\.")+"[\\W]";
-                String to_a1= " "+to+" ";
+                //escape dot character and add boundaries
+                String from_a1="(\\b|^)"+from.replace(".","\\.")+"(\\b|\\B|$)";
+                String to_a1=to;
 
                 //replace
                 text = text.replaceAll(from_a1,to_a1);
                 logger.debug(from_a1+"--->"+ to_a1);
+                //logger.debug("result of this replacement:"+text);
 
                 //escape dot, add boundaries and change first character to upper
                 String from_b1 = from.substring(0, 1).toUpperCase() + from.substring(1);
-                from_b1="[\\W]"+from_b1.replace(".","\\.")+"[\\W]";
+                from_b1="(\\b|^)"+from_b1.replace(".","\\.")+"(\\b|\\B|$)";
 
-                String to_b1=" "+to.substring(0, 1).toUpperCase() + to.substring(1)+" ";
+                String to_b1=to.substring(0, 1).toUpperCase() + to.substring(1);
 
                 //replace, but with first character uppercase
                 text = text.replaceAll(from_b1, to_b1);
@@ -93,14 +100,14 @@ public class AbbreviationDecorator extends TextInterfaceDecorator{
             else
             {
                 //add boundaries to match only whole words(to prevent replacements such as 'kadr'->'kadoktor')
-                String from_a2="\\b"+from+"\\b";
+                String from_a2="(\\b|^)"+from+"(\\b|$)";
 
                 //replace
                 text = text.replaceAll(from_a2,to);
                 logger.debug(from_a2+"--->"+ to);
 
                 //add boundaries and change first character to upper
-                String from_b2 = "\\b"+from.substring(0, 1).toUpperCase() + from.substring(1)+"\\b";
+                String from_b2 = "(\\b|^)"+from.substring(0, 1).toUpperCase() + from.substring(1)+"(\\b|$)";
                 String to_b2 = to.substring(0, 1).toUpperCase() + to.substring(1);
 
                 //replace, but with uppercase first character
