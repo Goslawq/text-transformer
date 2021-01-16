b5d15078285734db13af8ccd4b6fe8f833628d55
szymon8576 <50836217+szymon8576@users.noreply.github.com>
2021-01-16 14:48:01 +0100

    updated javadoc info, some minor improvements

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -16,7 +16,8 @@ public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
     /**
-     * Binding responsible for handing GET requests
+     * Binding responsible for handling 'transform text' GET requests
+     * Mapping: /{text}
      * @param text input text
      * @param transforms array of strings describing wanted transformations
      * @return output Text after transformations
@@ -27,7 +28,7 @@ public class TextTransformerController {
         //stop spam in console, when browser requests site icon
         if (text.equals("favicon.ico"))
             return "";
-        logger.info("Got 'GET' request");
+        logger.info("Got 'GET' request (transform text)");
 
         // log the parameters
         logger.debug("Given text: "+text+" with parameters: "+Arrays.toString(transforms));
@@ -41,10 +42,16 @@ public class TextTransformerController {
         return output;
     }
 
+    /**
+     * Binding responsible for handling 'get available transforms' GET request
+     * Mapping: /transforms/
+     * @return output String containing names of available transforms
+     */
     @RequestMapping(value="/transforms/",method = RequestMethod.GET, produces = "application/json")
     public String get() {
 
-        logger.debug("got 'get availTransforms' request");
+
+        logger.debug("got GET request (get available transforms)");
 
         // perform the transformation, you should run your logic here
 
