9c075ca98c94f0d26f2e357636395d0d67cdf2b3
Jakub Goslawski <goslawek1234@gmail.com>
2021-01-16 13:50:55 +0100

    fixed packaging of Client

---
+++ TextTransformerClient/src/main/java/pl/put/poznan/TextTransformerClientWrapper.java
@@ -0,0 +1,9 @@
+package pl.put.poznan;
+
+public class TextTransformerClientWrapper {
+
+    public static void main(String[] args){
+        TextTransformerClientApplication.main(args);
+    }
+
+}
