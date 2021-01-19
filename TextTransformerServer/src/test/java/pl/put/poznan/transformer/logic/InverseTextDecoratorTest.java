2ebb542b7d75a6a7fa4d60949446bedd22167959
Eliza Czaplewska <czaplewska.eliza@gmail.com>
2021-01-19 17:37:38 +0100

    Add mock test InverseTextDecorator

--- TextTransformerServer/src/test/java/pl/put/poznan/transformer/logic/InverseTextDecoratorTest.java
+++ TextTransformerServer/src/test/java/pl/put/poznan/transformer/logic/InverseTextDecoratorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class InverseTextDecoratorTest {
     TextInterface transformer = null;
@@ -19,6 +21,15 @@ class InverseTextDecoratorTest {
     void testInverse(){
         transformer = new ConcreteText("I am tesTing strinG!!!");
         input = new InverseTextDecorator(transformer);
+        assertEquals("!!!gnirtS gnitset Ma i", input.getTransformedText());
+    }
+
+    @Test
+    void mockTestInverse(){
+        TextInterface mock_interface = mock(TextInterface.class);
+        when(mock_interface.getTransformedText()).thenReturn("I am tesTing strinG!!!");
+
+        input = new InverseTextDecorator(mock_interface);
         assertEquals("!!!gnirtS gnitset Ma i", input.getTransformedText());
     }
 
