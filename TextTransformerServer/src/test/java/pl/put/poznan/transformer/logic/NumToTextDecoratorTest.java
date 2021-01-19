ee2d3673396e21afb1181a5c33312fdaa1927125
Wrona13 <wradzi@web.de>
2021-01-19 18:17:20 +0100

    mocki

--- TextTransformerServer/src/test/java/pl/put/poznan/transformer/logic/NumToTextDecoratorTest.java
+++ TextTransformerServer/src/test/java/pl/put/poznan/transformer/logic/NumToTextDecoratorTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class NumToTextDecoratorTest{
     TextTransformerServer _decorator;
+    NumToTextDecorator nt_decorator;
 
     @BeforeEach
     void setUp() {
@@ -17,32 +21,64 @@ public class NumToTextDecoratorTest{
     }
 
     @Test
-    void testDeleteDoubleDecorator_1(){
+    void testNumToTextDecorator_1(){
         _decorator = new TextTransformerServer(new String[]{"number"});
         assertEquals("cz3ść", _decorator.transform("cz3ść"));
     }
 
     @Test
-    void testDeleteDoubleDecorator_2(){
+    void testNumToTextDecorator_2(){
         _decorator = new TextTransformerServer(new String[]{"number"});
         assertEquals("siedemdziesiąt siedem", _decorator.transform("77"));
     }
 
     @Test
-    void testDeleteDoubleDecorator_3(){
+    void testNumToTextDecorator_3(){
         _decorator = new TextTransformerServer(new String[]{"number"});
         assertEquals("99999999999999999999", _decorator.transform("99999999999999999999"));
     }
 
     @Test
-    void testDeleteDoubleDecorator_4(){
+    void testNumToTextDecorator_4(){
         _decorator = new TextTransformerServer(new String[]{"number"});
         assertEquals("osiem siedem osiem", _decorator.transform("8 siedem 8"));
     }
 
     @Test
-    void testDeleteDoubleDecorator_5(){
+    void testNumToTextDecorator_5(){
         _decorator = new TextTransformerServer(new String[]{"number"});
         assertEquals("trzydzieści trzy tysiące", _decorator.transform("33 tysiące"));
+    }
+
+    @Test
+    void testNumToTextDecoratorMock_1(){
+        TextInterface mock_textInterface=mock(TextInterface.class);
+        when(mock_textInterface.getTransformedText()).thenReturn("33 tysiące");
+        nt_decorator =new NumToTextDecorator(mock_textInterface);
+        assertEquals("trzydzieści trzy tysiące", nt_decorator.getTransformedText());
+    }
+
+    @Test
+    void testNumToTextDecoratorMock_2(){
+        TextInterface mock_textInterface=mock(TextInterface.class);
+        when(mock_textInterface.getTransformedText()).thenReturn("99999999999999999999");
+        nt_decorator =new NumToTextDecorator(mock_textInterface);
+        assertEquals("99999999999999999999", nt_decorator.getTransformedText());
+    }
+
+    @Test
+    void testNumToTextDecoratorMock_3(){
+        TextInterface mock_textInterface=mock(TextInterface.class);
+        when(mock_textInterface.getTransformedText()).thenReturn("8 siedem 8");
+        nt_decorator =new NumToTextDecorator(mock_textInterface);
+        assertEquals("osiem siedem osiem", nt_decorator.getTransformedText());
+    }
+
+    @Test
+    void testNumToTextDecoratorMock_4(){
+        TextInterface mock_textInterface=mock(TextInterface.class);
+        when(mock_textInterface.getTransformedText()).thenReturn("nic");
+        nt_decorator =new NumToTextDecorator(mock_textInterface);
+        assertEquals("nic", nt_decorator.getTransformedText());
     }
 }
--- TextTransformerServer/src/test/java/pl/put/poznan/transformer/logic/ReverseCaseDecoratorTest.java
+++ TextTransformerServer/src/test/java/pl/put/poznan/transformer/logic/ReverseCaseDecoratorTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.*;
+import static org.mockito.Mockito.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReverseCaseDecoratorTest {
     TextTransformerServer _decorator;
+    ReverseCaseDecorator rc_decorator;
 
     @BeforeEach
     void setUp() {
@@ -46,6 +50,39 @@ public class ReverseCaseDecoratorTest {
     void testReverseCaseDecorator_5(){
         _decorator = new TextTransformerServer(new String[]{"reverse"});
         assertEquals("Małe", _decorator.transform("mAŁE"));
+    }
+
+    @Test
+    void testReverseCaseDecoratorMock_1(){
+        TextInterface mock_textInterface=mock(TextInterface.class);
+        when(mock_textInterface.getTransformedText()).thenReturn("Hi");
+        rc_decorator =new ReverseCaseDecorator(mock_textInterface);
+        assertEquals("hI", rc_decorator.getTransformedText());
+    }
+
+    @Test
+    void testReverseCaseDecoratorMock_2(){
+        TextTransformerServer mock_decorator = new TextTransformerServer(new String[]{"reverse"});
+        assertEquals("czeSC WszYstkim", mock_decorator.transform("CZEsc wSZySTKIM"));
+    }
+
+    @Test
+    void testReverseCaseDecoratorMock_3(){
+        TextTransformerServer mock_decorator = new TextTransformerServer(new String[]{"reverse"});
+        assertEquals("HaLo", mock_decorator.transform("hAlO"));
+    }
+
+    @Test
+    void testReverseCaseDecoratorMock_4(){
+        TextTransformerServer mock_decorator = new TextTransformerServer(new String[]{"reverse"});
+        assertEquals("DUZE", mock_decorator.transform("duze"));
+    }
+
+    @Test
+    void testReverseCaseDecoratorMock_5(){
+        TextTransformerServer mock_decorator = new TextTransformerServer(new String[]{"reverse"});
+        assertEquals("Małe", mock_decorator.transform("mAŁE"));
+        //verify(mock_decorator).transform(anyString());
     }
 
 
